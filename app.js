e3fb132629234082cdd2b5c6fb02301e852f0aeb
JojoDuke <jojoamankwa@gmail.com>
2022-10-09 15:58:45 +0000

    saa

--- app.js
+++ app.js
@@ -43,6 +43,6 @@ const params = JSON.stringify({
       })
   });
 
-  app.listen(process.env.PORT || 4000, () => {
+  app.listen(process.env.PORT || 4000, '0.0.0.0', () => {
     console.log(`Server is running on port 4000`);
   })
